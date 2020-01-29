8590501bd28c74fdce4d457382d3abb5611abab9
AmosMwangi <amosmwangi101@gmail.com>
2020-01-29 09:33:13 +0300

    made view for user

--- crop/views.py
+++ crop/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User
+# from rest_framework import generics, status
+from .serializers import *
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
+​
+class UserList(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+​
+​
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.validated_data['password'] = make_password(serializer.validated_data['password'])
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
