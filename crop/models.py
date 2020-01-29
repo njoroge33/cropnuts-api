65f88dd7ec797bf7e6d50c3c59c02ac5f4e37ca1
AmosMwangi <amosmwangi101@gmail.com>
2020-01-29 10:14:24 +0300

    imported django user

--- crop/models.py
+++ crop/models.py
@@ -1,9 +1,3 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(max_length=60)
-    # password = forms.CharField(max_length=32, widget=forms.PasswordInput)
-
-    def __str__(self):
-        return self.username
