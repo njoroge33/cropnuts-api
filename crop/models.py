e1504d293e5dfe6c0f4c081849c9aaf569fe4c5b
vique254 <victoronyango051@gmail.com>
2020-01-29 10:25:26 +0300

    create crate model

--- crop/models.py
+++ crop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import datetime
+YEAR_CHOICES = []
+for r in range(1980, (datetime.datetime.now().year+1)):
+    YEAR_CHOICES.append((r,r))
 # Create your models here.
+
+class Crate(models.Model):
+    country = models.CharField(max_length=50)
+    year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    
+    def __str__(self):
+       return self.country  
